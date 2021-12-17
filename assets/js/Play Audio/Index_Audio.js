70dd6b03998c9df038f976707835910652c0f9c8
Varun Kumar <varun.kumar@xaviers.edu.in>
2021-12-17 20:53:31 +0530

    Added audio to the buttion

--- assets/js/Play Audio/Index_Audio.js
+++ assets/js/Play Audio/Index_Audio.js
@@ -1,6 +1,28 @@
-var indexaudio;
-indexaudio = document.getElementById( "IndexTitleAudio" );
-function playaudio()
+var Audiotitleindiex = new Audio();
+Audiotitleindiex.src = "assets/audio/Index Title.wav";
+
+var AudioKeyterms = new Audio();
+AudioKeyterms.src = "assets/audio/key Values index.wav";
+
+var AudioVisionIndex = new Audio();
+AudioVisionIndex.src = "assets/audio/Vision Indiex.wav";
+
+var Audioinclusion = new Audio();
+Audioinclusion.src = "assets/audio/Fostering Inclusion.wav";
+
+function playAudiotitle()
 {
-    indexaudio.play();
+    Audiotitleindiex.play();
+}
+function PlayAudioKeyterms()
+{
+    AudioKeyterms.play();
+}
+function PlayVisionIndex()
+{
+    AudioVisionIndex.play();
+}
+function PlayInclusion()
+{
+    Audioinclusion.play();
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,5 @@ var bt_heretohelp;
 bt_heretohelp = document.getElementById( "Here2Help" );
 bt_heretohelp.onclick = function ()
 {
-    location.href = "Here2Help.html";
+    location.href = "Pages/Here2Help.html";
 }
